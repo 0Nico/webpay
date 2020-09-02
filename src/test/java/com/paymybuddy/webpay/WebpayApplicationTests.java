47b431433fcb753345b3f4be1213c86bb84f6ed9
Nicolas Dupuy <nicolas.dupuy@mimbus.com>
2020-09-02 18:24:53 +0200

    Project initialization

---
+++ src/test/java/com/paymybuddy/webpay/WebpayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paymybuddy.webpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebpayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
